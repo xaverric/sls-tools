6ffae1b1e73571bf6fe70d58c1d85fb72a617f6b
Daniel Jílek <jilek.daniel@gmail.com>
2023-11-15 11:17:49 +0100

    release/1.9.6

--- app/command/export/main/visualize/bookkit/bookkit-visualizer.js
+++ app/command/export/main/visualize/bookkit/bookkit-visualizer.js
@@ -7,15 +7,19 @@ const {isVisualizable} = require("../helper/visualization-helper");
 
 const handleBookkitVisualization = async (exportItem, configuration) => {
     if (isVisualizable(exportItem)) {
-        CONSOLE_LOG.info(`Visualizing ${exportItem.name}`)
-        let data = await readDataForVisualization(exportItem);
-        if (!data) {
-            CONSOLE_LOG.warn("Nothing to visualize, empty data gathered.")
-            return;
+        try {
+            CONSOLE_LOG.info(`Visualizing ${exportItem.name}`)
+            let data = await readDataForVisualization(exportItem);
+            if (!data) {
+                CONSOLE_LOG.warn("Nothing to visualize, empty data gathered.")
+                return;
+            }
+            let uu5StringBuilderFnc = decideUu5StringTemplateBuilder(exportItem.visualize.type);
+            let uu5StringContent = uu5StringBuilderFnc(exportItem, data);
+            await updateSection(configuration.bookkit.uri, exportItem.visualize.page, exportItem.visualize.code, uu5StringContent, configuration.bookkit.token);
+        } catch (e) {
+            CONSOLE_LOG.error(`Visualizing of ${exportItem.name} failed. ${e}`);
         }
-        let uu5StringBuilderFnc = decideUu5StringTemplateBuilder(exportItem.visualize.type);
-        let uu5StringContent = uu5StringBuilderFnc(exportItem, data);
-        await updateSection(configuration.bookkit.uri, exportItem.visualize.page, exportItem.visualize.code, uu5StringContent, configuration.bookkit.token);
     }
 }
 
